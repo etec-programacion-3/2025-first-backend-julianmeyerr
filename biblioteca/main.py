12104779db23b0af9c72f78ce77b6f3cb3b67a57
ETec <sc.sabatini@alumno.etec.um.edu.ar>
2025-04-23 12:56:05 -0300

    Adición del sistema de usuarios y autenticacion

--- biblioteca/main.py
+++ biblioteca/main.py
@@ -1,9 +1,11 @@
-from models import Libro
+from models import Libro, Usuario, IniciarSesion
 from fastapi import FastAPI
 
 app=FastAPI()
 
 libros_db = []
+usuarios_db = []
+rol = str("invitado")
 
 ### CREAR LIBRO
 @app.post("/libros/",response_model=Libro)
@@ -12,13 +14,11 @@ def crear_libro(nuevo_libro:Libro):
     return nuevo_libro
 ###
 
-
 ### OBTENER TODOS LOS LIBROS
 @app.get("/libros/",response_model=list[Libro])
 def obtener_libros():
     return libros_db
 ###
-
 
 ### OBTENER LIBRO POR ID
 @app.get("/libros/{libro_id}",response_model=Libro)
@@ -29,7 +29,6 @@ def obtener_libro(libro_id: int):
     return {"ERROR" : "Libro no encontrado"}
 ###
 
-
 ### BORRAR LIBRO
 @app.delete("/libros/{libro_id}")
 def eliminar_libro(libro_id: int):
@@ -38,4 +37,24 @@ def eliminar_libro(libro_id: int):
             libros_db.remove(x)
             return {"Mensaje" : "Libro eliminado"}
     return {"ERROR" : "Libro no encontrado"}
+###
+
+
+###REGISTRAR USUARIO
+@app.post("/usuarios/",response_model=Usuario)
+def registrar_usuario(nuevo_usuario:Usuario):
+    usuarios_db.append(nuevo_usuario)
+    return nuevo_usuario
+###
+
+### INICAR SESION
+@app.post("/usuarios/login")
+def iniciar_sesion(usuario:IniciarSesion):
+    for x in usuarios_db:
+        if x.id == usuario.id:
+            if x.contrasena == usuario.contrasena:
+                rol= x.rol
+                return {"Mensaje" : "Inicio de sesión exitoso"}
+            return {"ERROR" : "Contraseña incorrecta"}
+    return {"ERROR" : "Usuario no encontrado"}
 ###
--- biblioteca/models.py
+++ biblioteca/models.py
@@ -6,3 +6,14 @@ class Libro(BaseModel):
     autor: str
     anio: int
     genero: str
+
+class Usuario(BaseModel):
+    id: int
+    nombre: str
+    contrasena: str
+    rol: str
+    status: str
+
+class IniciarSesion(BaseModel):
+    id: int
+    contrasena: str
