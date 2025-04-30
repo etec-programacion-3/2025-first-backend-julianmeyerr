2c751322ea559b9d7e2dd9f4d707f5413788a3df
ETec <sc.sabatini@alumno.etec.um.edu.ar>
2025-04-30 12:48:37 -0300

    finalizacion fase 2

--- biblioteca/main.py
+++ biblioteca/main.py
@@ -1,5 +1,5 @@
 from models import Libro, Usuario, IniciarSesion
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import random
 
 app=FastAPI()
@@ -13,10 +13,12 @@ def crear_libro(nuevo_libro:Libro,token:int):
     for x in usuarios_db:
         if x.token == token:
             if x.rol == "admin":
+
                 libros_db.append(nuevo_libro)
                 return nuevo_libro
-            return {"ERROR" : "No tiene permisos para crear libros"}
-    return {"ERROR" : "Token inválido"}
+            
+            raise HTTPException(status_code=403, detail="No tiene permisos para crear libros")
+    raise HTTPException(status_code=401, detail="Token inválido")
 ###
 
 ### OBTENER TODOS LOS LIBROS
@@ -31,7 +33,8 @@ def obtener_libro(libro_id: int):
     for x in libros_db:
         if x.id == libro_id:
             return x
-    return {"ERROR" : "Libro no encontrado"}
+        
+    raise HTTPException(status_code=404, detail="Libro no encontrado")
 ###
 
 ### BORRAR LIBRO
@@ -47,10 +50,11 @@ def eliminar_libro(libro_id: int,token:int):
                         return {"Mensaje" : "Libro eliminado"}
                 return {"ERROR" : "Libro no encontrado"}
             
-            return {"ERROR" : "No tiene permisos para eliminar libros"}
-    return {"ERROR" : "Token inválido"}
+            raise HTTPException(status_code=403, detail="No tiene permisos para eliminar libros")
+    raise HTTPException(status_code=401, detail="Token inválido")   
 ###
 
+#####################################USUARIOS#####################################
 
 ###REGISTRAR USUARIO
 @app.post("/usuarios",response_model=Usuario)
@@ -66,31 +70,50 @@ def iniciar_sesion(usuario:IniciarSesion):
         if x.id == usuario.id:
             if x.contrasena == usuario.contrasena:
                 x.token = random.randint(100, 999)
-                return {"Mensaje" : "Inicio de sesión exitoso\nSu token es: " + str(x.token)}
+                return {"Mensaje" : "Inicio de sesión exitoso. Su token es: " + str(x.token)}
             return {"ERROR" : "Contraseña incorrecta"}
-    return {"ERROR" : "Usuario no encontrado"}
+        
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
+###
+
+### CERRAR SESION
+@app.post("/usuarios/logout/{token}")
+def cerrar_sesion(token:int):
+    for x in usuarios_db:
+        if x.token == token:
+            x.token = None
+            return {"Mensaje" : "Sesión cerrada exitosamente"}
+    raise HTTPException(status_code=401, detail="Token inválido")
 ###
 
 ### OBTENER TODOS LOS USUARIOS
-@app.get("/usuarios/{token}",response_model=list[Usuario])
-def obtener_usuarios(token:int):
+@app.get("/usuarios/{token}",response_model=list[Usuario| dict])
+def obtener_usuarios(token :int):
     for x in usuarios_db:
         if x.token == token:
             if x.rol == "admin":
                 return usuarios_db
-            return {"ERROR" : "No tiene permisos para ver usuarios"}
-    return {"ERROR" : "Token inválido"}
+            else:
+                lista = []
+                for y in usuarios_db:
+                    lista.append({"id": y.id, "nombre": y.nombre, "rol": y.rol})
+                return lista
+    raise HTTPException(status_code=401, detail="Token inválido")
 ###
 
 ### OBTENER USUARIO POR ID
-@app.get("/usuarios/{usuario_id}/{token}",response_model=Usuario)
+@app.get("/usuarios/{usuario_id}/{token}",response_model=Usuario | dict)
 def obtener_usuario(usuario_id: int,token:int):
     for x in usuarios_db:
-        if x.token == token:
-            if x.rol == "admin":
-                for y in usuarios_db:
-                    if y.id == usuario_id:
-                        return y
-                return {"ERROR" : "Usuario no encontrada"}
-            return {"ERROR" : "No tiene permisos para ver usuarios"}
-    return {"ERROR" : "Token inválido"}
+        if x.id == usuario_id:
+            for y in usuarios_db:
+                if y.token == token:
+                    if y.rol == "admin":
+                        return x
+                    else:
+                        return {"id": x.id, "nombre": x.nombre, "rol": x.rol}
+                    
+            raise HTTPException(status_code=401, detail="Token inválido")
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
+###
+
